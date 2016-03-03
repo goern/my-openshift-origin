74a3685473407f875c17fcc5cceb50f215bc7fbd
Christoph Görn <goern@redhat.com>
2016-03-03 20:32:57 +0100

    setting the right perms and ownership

--- configure-nfs-server.sh
+++ configure-nfs-server.sh
@@ -5,6 +5,10 @@
 sudo yum install -y nfs-utils
 
 sudo mkdir -p /srv/nfs/registry /srv/nfs/vol0001 /srv/nfs/vol0002 /srv/nfs/vol0003
+sudo chmod 777 /srv/nfs/registry /srv/nfs/vol0001 /srv/nfs/vol0002 /srv/nfs/vol0003
+sudo chown nfsnobody:nfsnobody /srv/nfs/registry /srv/nfs/vol0001 /srv/nfs/vol0002 /srv/nfs/vol0003
+setsebool -P virt_use_nfs 1
+
 sudo chmod 666 /etc/exports
 echo '/srv/nfs/registry *(rw,sync,no_root_squash,no_all_squash)' >>/etc/exports
 echo '/srv/nfs/vol0001 *(rw,sync,no_root_squash,no_all_squash)' >>/etc/exports
@@ -12,7 +16,6 @@ echo '/srv/nfs/vol0002 *(rw,sync,no_root_squash,no_all_squash)' >>/etc/exports
 echo '/srv/nfs/vol0003 *(rw,sync,no_root_squash,no_all_squash)' >>/etc/exports
 sudo chmod 644 /etc/exports
 sudo exportfs -av
-setsebool -P virt_use_nfs 1
 
 cat <<EOT >>/etc/sysconfig/nfs
 LOCKD_TCPPORT=32803
